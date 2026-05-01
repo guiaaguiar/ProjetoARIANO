d6b0cffdfaada2e0faca70d98d4500297192c2d7
Guilherme Aguiar <guilhermeada81@gmail.com>
2026-05-01 19:43:10 -0300

    fix: atualiza e substitui o favicon oficial do projeto ariano

--- frontend/src/components/StackedLogo.tsx
+++ frontend/src/components/StackedLogo.tsx
@@ -1,4 +1,4 @@
-import FavIcon from './layout/Fav Icon.png';
+import FavIcon from './layout/favicon.png';
 
 export const StackedLogo = ({ size = 16 }: { size?: number; color?: string }) => (
   <img src={FavIcon} alt="ARIANO Logo" width={size} height={size} style={{ display: 'inline-block' }} />
